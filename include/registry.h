92566d702b6cb5c28c70d184207734307af0f461
Багрянский Андрей <andrey@1dist.ru>
2023-11-03 21:02:45 +0300

    1

--- include/registry.h
+++ include/registry.h
@@ -15,14 +15,23 @@ public:
 template<typename T1, typename T2>
 class Registry{
 public:
-    void add(T1 inFirst, T2 inSecond){
-        Rpair<T1, T2>* rpair = new Rpair(inFirst, inSecond);
-        this->reg.push_back(*rpair);
-        delete rpair;
-    }
+    void add(T1 inFirst, T2 inSecond);
+    void remove(T1 inKey);
+    void print();
+    void find(T1 inKey);
+private:
+    std::vector<Rpair<T1, T2>> reg;
+};
 
-//template<typename T1, typename T2>
-void remove(T1 inKey){
+template<typename T1, typename T2>
+void Registry<T1, T2>::add(T1 inFirst, T2 inSecond){
+    Rpair<T1, T2>* rpair = new Rpair(inFirst, inSecond);
+    this->reg.push_back(*rpair);
+    delete rpair;
+}
+
+template<typename T1, typename T2>
+void Registry<T1, T2>::remove(T1 inKey){
     for(auto it = reg.begin(); it != reg.end();){
         if (it->first == inKey){
             it = reg.erase(it);
@@ -34,16 +43,27 @@ void remove(T1 inKey){
     }
 }
 
-//template<typename T1, typename T2>
-void print(){
+template<typename T1, typename T2>
+void Registry<T1, T2>::print(){
     if(reg.empty()){
         std::cout << "The registry is empty!" <<std::endl;
     }else{
-        for(int i = 0; i < reg.size; ++i){
+        for(int i = 0; i < reg.size(); ++i){
             std::cout << reg[i].first << " " << reg[i].second << std::endl;
         }
     }
 }
-private:
-    std::vector<Rpair<T1, T2>> reg;
-};
+
+template<typename T1, typename T2>
+void Registry<T1, T2>::find(T1 inKey){
+    int count =0;  
+    for(auto it = reg.begin(); it != reg.end(); ++it){
+        if (it->first == inKey){
+           std::cout << it->second << " ";
+           ++count;
+        }
+    }
+    if(count == 0) std::cout << "Key is not found!";
+    std::cout << std::endl;    
+}
+
--- main.cpp
+++ main.cpp
@@ -5,8 +5,26 @@
 
 int main(){
     Registry<int, int> reg1;
-    //Rpair<int, int> pair(10,10);
+    std::cout << "Step1\n";
     reg1.add(1, 10);
-    //registry.print();
-    std::cout << " ";
+    reg1.add(25, 48);
+    reg1.add(1, 48);
+    reg1.print();
+
+    std::cout << "Step2\n";
+    reg1.remove(1);
+    reg1.print();
+
+
+    Registry<std::string, int> reg2;
+    std::cout << "Step3\n";
+    reg2.add("banana", 10);
+    reg2.add("apple", 5);
+    reg2.add("banana", 47);
+    reg2.print();
+
+    std::cout << "Step4\n";
+    reg2.find("banana");
+    std::cout << "Step5\n";
+    reg2.find("potato");
 }
